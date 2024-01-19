397519b953826e8df460323d747754ef0ff556da
KGH <kghui200@naver.com>
2024-01-19 16:05:45 +0900

    chart lib add, spec sheet

--- js/function.js
+++ js/function.js
@@ -1,9 +1,11 @@
 
-window.addEventListener('DOMContentLoaded', function(){
+// window.addEventListener('DOMContentLoaded', function(){
 
 
-const $test = document.querySelector('.topview');
-$test.style.backgroundImage = url('/img/down/10788-ryzen-master-image-1260x709.webp')
-console.log($test.style.backgroundImage);
+// // const $test = document.querySelector('.topview');
+// // console.log($test.style);
+// $topview = document.querySelector('.topview');
+// console.log($topview);
+// $topview.style = 'background-image: url(./img/coverup.jfif)';
 
-});
+// });
