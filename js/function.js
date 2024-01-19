3051cad508ef0e25a8bbd1e4c107d75b277ddd89
KGH <kghui200@naver.com>
2024-01-19 14:07:30 +0900

    chart css

---
+++ js/function.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('DOMContentLoaded', function(){
+
+
+const $test = document.querySelector('.topview');
+$test.style.backgroundImage = url('/img/down/10788-ryzen-master-image-1260x709.webp')
+console.log($test.style.backgroundImage);
+
+});
