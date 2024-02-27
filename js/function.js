ff70d02d726b25419e14e03667f86c6f042ed69d
Kascald <kghui20@gmail.com>
2024-02-28 01:43:25 +0900

    ...sidebar making...

--- js/function.js
+++ js/function.js
@@ -31,7 +31,7 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     const $articles_ = document.querySelectorAll("section > article");
     const $middle_menu_items = document.querySelectorAll("header>.headcont>nav>.navCon>.gnb>li");
-    const $middle_menu_ = document.querySelector("header>.headcont>nav>.navCon>.gnb");
+    const $middle_menu_ = document.querySelector("header>.headcont>nav");
     const $sideBar = document.querySelector("aside>.sideBar");
 
 
@@ -64,28 +64,40 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     window.addEventListener('scroll',()=>{
         let $screenSize = document.body.offsetWidth;
+
+        let $wSize = $screenSize < 1300? 0 :$screenSize;
+
         console.log("현재 스크린 너비 : ",$screenSize);
-        console.log("예상 사이드바 이동거리 : ",$screenSize * 0.2,"px");
+        
+
+       
         let $nowScrollY = document.querySelector('html').scrollTop;
         console.log("현재 스크롤 Y : ",$nowScrollY);
-        sideBar_toggle($nowScrollY,$screenSize);
+        sideBar_toggle($nowScrollY,$wSize);
         
     });
 
 
     //FIXME 사이드바 스크롤 액션 고치기
-    function sideBar_toggle(scrollY,wSize) {
-
+    function sideBar_toggle(scrollY,windowSize) {
+        let wSize = parseInt(windowSize);
+        console.log("적용 스크린 너비 : ",wSize);
+        console.log("예상 사이드바 이동거리 : ",Math.floor(wSize * 0.2),"px");
         if(scrollY > $middle_menu_.offsetTop){
             // console.log("현재 스크롤Y가 section article_1을 넘어섰습니다.");
             // console.log("$sideBar.style.display 를 block으로 변경합니다.");
-            $sideBar.style =`display: block; transform: translateX(${wSize * -0.1}px)`;
+            $sideBar.style =`display: block; transform: translateX(${Math.floor(wSize * -0.2)}%); transition: all 1.3s ease-in-out;`;
 
-        }else if(scrollY === 0) {
+        }else if(scrollY < $middle_menu_.offsetTop) {
             // console.log("현재 스크롤Y가 section article_1보다 작습니다.");
             // console.log("$sideBar.style.display 를 이동시킨 후 none으로 변경합니다.");
-            $sideBar.style =`transform: translateX((${wSize * 0.1}px)`;
-            // setTimeout(()=>{$sideBar.style='display:none;'},1300);
+
+            $sideBar.style =`display: none; transform: translateX((${Math.floor(wSize * 0.2)}%); transition: all 1.3s ease-in-out;`;
+            // let delayStlye = setTimeout(function() {
+            //     $sideBar.style='display:none;'}
+            //     ,1300);
+            
+            // clearTimeout(delayStlye);
         }
     }
    
