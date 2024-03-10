50f07d626556be5ba6e59f9b07f0c4d2a00ec66c
YRN <118609415+Kascald@users.noreply.github.com>
2024-03-10 21:38:02 +0900

    JS- Next/ Prev Btn add

--- js/function.js
+++ js/function.js
@@ -45,7 +45,90 @@ window.addEventListener("DOMContentLoaded", () => {
     // console.log();
     // console.log();
 
-    //For article-offsetY save
+    let vid_oldidx;
+    let vid_nowidx = 0;
+
+    //For graph scroll action
+    let graph_oldidx;
+    let graph_nowidx = 0;
+
+    var sideBar = true;
+    init();
+    
+    function scrollEvent() {
+        let 
+    }
+     
+    // window.addEventListener('scroll', () => {
+       
+    //     }
+      
+    // });
+
+  
+
+    //TODO 유투브 영상 이전,다음버튼 동작 - 다음 영상으로 넘기는 슬라이드 구현 
+    const $ytPrevBtn = document.querySelector('.vid_prevBtn > a');   //이전버튼
+    const $ytNextBtn = document.querySelector('.vid_nextBtn > a');  //다음버튼
+    const $ytViewWindow = document.querySelector('.yt_viewWindow'); //동영상 뭉치 윈도우
+    
+    const $ytVid1 = document.querySelector('.yt_viewWindow > #vid_1');
+    const $ytVid2 = document.querySelector('.yt_viewWindow > #vid_2');
+    const $ytVid3 = document.querySelector('.yt_viewWindow > #vid_3');
+
+
+
+    $ytNextBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        // console.log(this.innerWidth);
+        console.log($ytViewWindow.scrollLeft);
+       
+        if (Number($ytViewWindow.scrollLeft) == 1220 ) {
+            $ytViewWindow.scrollBy({
+                top: 0,
+                left: -1220,
+                behavior : "smooth",
+            });
+        } else {
+        $ytViewWindow.scrollBy({
+            top: 0,
+            left: 610,
+            behavior: "smooth",
+          });
+        }
+    });// nextBtn action END
+
+    $ytPrevBtn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        // console.log(this.innerWidth);
+        console.log(Number($ytViewWindow.scrollLeft) == 0 );
+        if (Number($ytViewWindow.scrollLeft) == 0) {
+            $ytViewWindow.scrollBy({
+                top: 0,
+                left: 1220,
+                behavior: "smooth",
+              });
+        }else {
+        $ytViewWindow.scrollBy({
+            top: 0,
+            left: -610,
+            behavior: "smooth",
+          });
+        }
+    });// prevBtn action END
+
+
+    const sideBar_toTop = document.querySelector('.top_arrow');
+    sideBar_toTop.addEventListener('click',(e)=>{
+        e.preventDefault();
+        window.scrollBy({top: window.offsetTop, behavior: 'smooth'})
+    },{ capture:false, passive : true});
+
+
+
+
+    function init() {
+        //For article-offsetY save
     let article_page_y = [];
     $articles_.forEach(($article) => {
         let article_Y = $article.offsetTop;
@@ -54,12 +137,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     console.log(article_page_y);
     //For video scroll action
-    let vid_oldidx;
-    let vid_nowidx = 0;
-
-    //For graph scroll action
-    let graph_oldidx;
-    let graph_nowidx = 0;
+    
 
     // Top Navigation 클릭 비활성화
     //Header 메뉴 클릭 비활성화1
@@ -85,10 +163,14 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    }
+
+      
     //화면 스크롤에 따른 사이드바 표시 여부 결정
-    var sideBar = true;
-    window.addEventListener('scroll', () => {
+    
+    function scrollAction(sideBar) {
         let scrollY = this.scrollY;
+        console.log(this.scrollY);
         let windowSize = window.innerWidth;
         $sideBar.style.display = 'block';
         $sideBar.style.transition = 'all 1.2s ease-in-out;';
@@ -102,18 +184,18 @@ window.addEventListener("DOMContentLoaded", () => {
             let HA1 = setTimeout(($sideBar.style.display = 'block'),1000);
             clearTimeout(HA1);
             sideBar = false;
+            //TODO sideBar 위치 일정값 넘어가면 display none
             let HA2 = setTimeout({
                 if(sideBar) {
                     $sideBar.style.display='none'
                 }
             },3000);
             clearTimeout(HA2);
+              //TODO sideBar 위치가 '숨김위치'로 이동한 후에 display:none; 처리 고민해보기
+        // 잘 작동되는지 log 찍을 방법 고민하기  x 위치가 window.innerWidth를 넘어가면 display:none?
         }
-        //TODO sideBar 위치가 '숨김위치'로 이동한 후에 display:none; 처리 고민해보기
-        // 잘 작동되는지 log 찍을 방법 고민하기
-    });
+    }
 
-    
     //사이드바 표시액션 메서드
     function sideBar_ViewAction(winSize) {
         let windowSize = parseInt(winSize);  //현재 윈도우 사이즈 받아서 parseInt
@@ -123,11 +205,10 @@ window.addEventListener("DOMContentLoaded", () => {
     function sideBar_HideAction(winSize) {
         let windowSize = parseInt(winSize);  //현재 윈도우 사이즈 받아서 parseInt
         $sideBar.style = `transform: translateX((${Math.floor(windowSize * 0.1)}%);`;
+
+        // if($sideBar.offsetX)
     }
 
-    //TODO 유투브 영상 이전,다음버튼 동작 - 다음 영상으로 넘기는 슬라이드 구현 
 
-
-  // }
-
-})//end
+})
+//end
